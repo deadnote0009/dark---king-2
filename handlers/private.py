3001a5d62f9076dc5f77956ae43ff82fa4020a43
LEGENDARY-OS <97961618+LEGENDARY-OS@users.noreply.github.com>
2022-02-03 03:26:27 -0800

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -13,7 +13,7 @@ async def start_(client: Client, message: Message):
         photo=f"https://te.legra.ph/file/bb7e6f59b3db29b215446.jpg",
         caption=f"""**𝐓𝐡𝐢𝐬 𝐈𝐬 𝐀𝐝𝐯𝐚𝐧𝐜𝐞 🥀𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐌𝐮𝐬𝐢𝐜 🎶 𝐁𝐨𝐭 𝐑𝐮𝐧 𝐎𝐧 𝐏𝐫𝐢𝐯𝐚𝐭𝐞 🥀 𝐕𝐩𝐬 💫𝐒𝐞𝐫𝐯𝐞𝐫 🌎 𝐅𝐞𝐞𝐥 ❤️ 𝐇𝐢𝐠𝐡 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐮𝐬𝐢𝐜 🎧 𝐈𝐧 𝐕𝐜 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐁𝐲 = [Dhruba'𝘅𝗗 🚬❤️](https://t.me/DhrubaXD)
 
-𝐂𝐫𝐞𝐚𝐭𝐨𝐫 :- [✨ 𝗠𝗿'𝗦𝗺𝗢𝗸𝗲𝗿 🚬 💜](https://t.me/DhrubaXD)
+𝐂𝐫𝐞𝐚𝐭𝐨𝐫 :- [✨ 𝗠𝗿'Dhruba 🚬 💜](https://t.me/DhrubaXD)
 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 :- [✨ Group ❤️🎸](https://t.me/Legend_K_Userbot)
 𝐃𝐢𝐬𝐜𝐮𝐬𝐬 :- [✨  Tough 𝗖𝗹𝗮𝗻 🎧](https://t.me/Legend_K_Userbot)
 𝐒𝐨𝐮𝐫𝐜𝐞  :- [✨  𝗖𝗹𝗶𝗰𝗸 ☑️ 𝗥𝗲𝗽𝗼 🌍](https://github.com/LEGENDARY-OS/AIMusicX)
